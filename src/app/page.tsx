ea4848a4eeadcde947121b59adb5927fdf0a977b
Bad Boy <badboyforlife708@gmail.com>
2025-08-05 10:45:30 +0000

    make land on the webpage first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import UserDashboardView from "./dashboard/_components/UserDashboardView";
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/landing/Hero';
+import { Advantages } from '@/components/landing/Advantages';
+import { Services } from '@/components/landing/Services';
+import { Testimonials } from '@/components/landing/Testimonials';
+import { Faq } from '@/components/landing/Faq';
+import { Footer } from '@/components/Footer';
 
 export default function Home() {
   return (
-    <UserDashboardView />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <Advantages />
+        <Services />
+        <Testimonials />
+        <Faq />
+      </main>
+      <Footer />
+    </div>
   );
 }
